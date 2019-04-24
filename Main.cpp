1e679e44810704ba564aba07ad03e584f133f222
Sean Oberoi <xsean.o@gmail.com>
2019-04-24 15:23:34 -0700

    finish up fixing iteration issues

--- Main.cpp
+++ Main.cpp
@@ -37,10 +37,6 @@ int main()
 
     displayList(LL->toVector());
 
-    /*
-
-    displayList(LL->toVector());
-
     if (!(LL->contains(60)))
     {
         cout << "60 doesn't exist, let's add it!";
@@ -48,21 +44,14 @@ int main()
         displayList(LL->toVector());
     }
 
-    LL->getCurrentSize();
-
     cout << "Lets remove 30 and 40!";
     LL->removeNode(30);
     LL->removeNode(40);
     displayList(LL->toVector());
 
-    LL->getCurrentSize();
-
     cout << "Lets add another 20...";
     LL->addNode(20);
     displayList(LL->toVector());
-
-    LL->getCurrentSize();
-    */
 
     return 0;
 }
